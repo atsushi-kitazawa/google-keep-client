c3f0929640bc9e53b63b8da93ffc3cac3c0e5eb9
atsushi-kitazawa <atsushi.kitazawa.1987@gmail.com>
2023-03-21 22:47:45 +0900

    WIP: keep client.

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+
+	"google.golang.org/api/keep/v1"
+)
 
 func main() {
-	fmt.Println("Hello")
+	do()
+}
+
+func do() {
+	log.Println("start")
+
+	ctx := context.Background()
+	service, err := keep.NewService(ctx)
+	if err != nil {
+		log.Fatalf("failed to create service. err = %v", err)
+	}
+
+	res, err := service.Notes.List().Do()
+	if err != nil {
+		log.Fatalf("failed to call list(). err = %v", err)
+	}
+
+	log.Println(res.HTTPStatusCode)
+	log.Println(len(res.Notes))
 }
